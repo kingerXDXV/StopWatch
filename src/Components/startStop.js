f77b8b08ccf306301508ff8ea27a84172d312b9e
kingerXDXV <palhaniachandan@gmail.com>
2022-08-25 01:31:25 +0530

    third commit

--- src/Components/startStop.js
+++ src/Components/startStop.js
@@ -1,16 +1,51 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "../style/style.css"
 const StartStop= function(){
     const [start,setStart] = useState("Start");
+    const [s,setS] = useState(0);
+    const [m,setM] = useState(0);
+    const [h,setH] = useState(0);
+    const [flag, setFlag] = useState(false);
     const clickHandler = ()=>{
-        if(start==="Start")
+        if(start==="Start"){
         setStart("Stop");
-        else
+        setFlag(true);
+        }
+        else{
         setStart("Start");
+        setFlag(false);
+        }
     }
+
+    const resetHandler = ()=>{
+        setFlag(false);
+        clickHandler();
+        setS(0);
+        setM(0);
+        setH(0);
+    }
+
+    useEffect(()=>{
+        if(flag){
+            setTimeout(()=>{
+                setS((s)=>s+1);
+                    if(s===59){
+                        setM((m)=>m+1);
+                        setS(0);
+                    }
+                    if(m===59){
+                        setH((h)=>h+1)
+                        setM(0);
+                    }
+            },1000);
+        }
+    })
     return(
         <div>
+            
             <button onClick={clickHandler} className="btn1">{start}</button>
+            <button onClick={resetHandler}>Reset</button>
+            <label>hello {h}:{m}:{s}</label>
         </div>
     )
 }
